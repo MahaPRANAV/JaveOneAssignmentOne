7dc2352a39a6f4e2eb3cc81a81f5266ef612a523
MahaPRANAV <MAHALAKSHMI.RAMU@GMAIL.COM>
2017-02-22 19:01:50 +0530

    Add files via upload

---
+++ JavaOneAssignmentOne.java
@@ -0,0 +1,13 @@
+
+public class JavaOneAssignmentOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a = 2;
+     int b = 3; 
+	 int result ;
+	  result = a-(-b);
+	  System.out.println("Summation of two Integers "+result);
+	}
+
+}
